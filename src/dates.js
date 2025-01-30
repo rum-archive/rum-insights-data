b71a74a6291bcba1b8ce52330592f045746718b6
Nic Jansma <nic@nicj.net>
2025-01-30 13:09:01 -0500

    Auto generate dates

--- src/dates.js
+++ src/dates.js
@@ -1,3 +1,5 @@
+let moment = require("moment");
+
 // dateType:
 // recent_day: uses a single date from the last loaded month of data (first tuesday). e.g., (DATE = '2023-12-05')
 // recent_month: uses the last month of loaded data e.g., (DATE BETWEEN '2023-12-01' AND '2023-12-31')
@@ -55,120 +57,83 @@ function getDateArray( dateType ) {
         return "(DATE BETWEEN '2024-12-01' AND '2024-12-31')";
     }
     else if ( dateType === "first_days" ) {
-        const dates = [];
-        dates.push("2021-10-01");
-        dates.push("2021-11-01");
-        dates.push("2021-12-01");
-        dates.push("2022-01-01");
-        dates.push("2022-02-01");
-        dates.push("2022-03-01");
-        dates.push("2022-04-01");
-        dates.push("2022-05-01");
-        dates.push("2022-06-01");
-        dates.push("2022-07-01");
-        dates.push("2022-08-01");
-        dates.push("2022-09-01"); // from here on we can choose any date in the month. Keep it on 1 for now 
-        dates.push("2022-10-01");
-        dates.push("2022-11-01");
-        dates.push("2022-12-01");
-        dates.push("2023-01-01");
-        dates.push("2023-02-01");
-        dates.push("2023-03-01");
-        dates.push("2023-04-01");
-        dates.push("2023-05-01");
-        dates.push("2023-06-01");
-        dates.push("2023-07-01");
-        dates.push("2023-08-01");
-        dates.push("2023-09-01");
-        dates.push("2023-10-01");
-        dates.push("2023-11-01");
-        dates.push("2023-12-01");
-        dates.push("2024-01-01");
-        dates.push("2024-02-01");
-        dates.push("2024-03-01");
-        dates.push("2024-04-01");
-        dates.push("2024-05-01");
-        dates.push("2024-06-01");
-        dates.push("2024-07-01");
-        dates.push("2024-08-01");
-        dates.push("2024-09-01");
-        dates.push("2024-10-01");
-        dates.push("2024-11-01");
-        dates.push("2024-12-01");
-        dates.push("2025-01-01");
+        // date to start and today
+        let date = moment("2021-10-01");
+        let today = moment();
 
-        return dates; 
+        // push first date
+        const dates = [];
+        dates.push(date.format("YYYY-MM-DD"));
+
+        while (true) {
+            date = date.add(1, "months");
+
+            if (date.isAfter(today)) {
+                break;
+            }
+
+            dates.push(date.format("YYYY-MM-DD"));
+        }
+
+        return dates;
     }
     else if ( dateType === "first_tuesdays" ) {
-        const dates = [];
-        dates.push("2022-09-06");
-        dates.push("2022-10-04");
-        dates.push("2022-11-08");
-        dates.push("2022-12-06");
-        dates.push("2023-01-03");
-        dates.push("2023-02-07");
-        dates.push("2023-03-07");
-        dates.push("2023-04-04");
-        dates.push("2023-05-02");
-        dates.push("2023-06-06");
-        dates.push("2023-07-04");
-        dates.push("2023-08-08");
-        dates.push("2023-09-05");
-        dates.push("2023-10-03");
-        dates.push("2023-11-07");
-        dates.push("2023-12-05");
-        dates.push("2024-01-02");
-        dates.push("2024-02-06");
-        dates.push("2024-03-05");
-        dates.push("2024-04-02");
-        dates.push("2024-05-07");
-        dates.push("2024-06-04");
-        dates.push("2024-07-02");
-        dates.push("2024-08-06");
-        dates.push("2024-09-03");
-        dates.push("2024-10-01");
-        dates.push("2024-11-05");
-        dates.push("2024-12-03");
-        dates.push("2025-01-07");
+        // date to start and today
+        let date = moment("2022-09-06");
+        let today = moment();
 
-        return dates; 
+        // push first date
+        const dates = [];
+        dates.push(date.format("YYYY-MM-DD"));
+
+        while (true) {
+            // go to the next month
+            date = date.add(1, "months").startOf("month");
+
+            // find the first Tuesday
+            while (date.isoWeekday() !== 2) {
+                date.add(1, "day");
+            }
+
+            if (date.isAfter(today)) {
+                break;
+            }
+
+            dates.push(date.format("YYYY-MM-DD"));
+        }
+
+        return dates;
     }
     else if ( dateType === "first_and_third_tuesdays" ) {
-        const dates = getDateArray("first_tuesdays");
-        // now just add the third tuesdays
-        // NOTE!!! this assumes queries have ORDER BY DATE in them to properly interlace the dates!!
-        dates.push("2022-09-20");
-        dates.push("2022-10-03");
-        dates.push("2022-10-18");
-        dates.push("2022-11-22");
-        dates.push("2022-12-20");
-        dates.push("2023-01-17");
-        dates.push("2023-02-21");
-        dates.push("2023-03-21");
-        dates.push("2023-04-18");
-        dates.push("2023-05-16");
-        dates.push("2023-06-20");
-        dates.push("2023-07-18");
-        dates.push("2023-08-22");
-        dates.push("2023-09-19");
-        dates.push("2023-10-17");
-        dates.push("2023-11-21");
-        dates.push("2023-12-19");
-        dates.push("2024-01-16");
-        dates.push("2024-02-20");
-        dates.push("2024-03-19");
-        dates.push("2024-04-16");
-        dates.push("2024-05-21");
-        dates.push("2024-06-18");
-        dates.push("2024-07-16");
-        dates.push("2024-08-20");
-        dates.push("2024-09-17");
-        dates.push("2024-10-15");
-        dates.push("2024-11-19");
-        dates.push("2024-12-17");
-        dates.push("2025-01-21");
+        // date to start and today
+        let date = moment("2022-09-06");
+        let today = moment();
 
-        return dates; 
+        // push first date
+        const dates = [];
+        dates.push(date.format("YYYY-MM-DD"));
+
+        while (true) {
+            // go to the next month
+            date = date.add(1, "months").startOf("month");
+
+            // find the first Tuesday
+            while (date.isoWeekday() !== 2) {
+                date.add(1, "day");
+            }
+
+            if (date.isAfter(today)) {
+                break;
+            }
+
+            // first tuesday
+            dates.push(date.format("YYYY-MM-DD"));
+
+            // third tuesday
+            dates.push(moment(date).add(2, "weeks").format("YYYY-MM-DD"));
+        }
+
+        return dates;
     }
     else {
         throw new Error("dates:getDateArray: invalid dateType : " + dateType);
